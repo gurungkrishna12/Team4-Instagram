7d52d4b86caa946c8aa638b97943915bef9ddf2d
gurungkrishna12 <gurungkrishna.12@gmail.com>
2021-08-12 12:02:51 -0700

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 class App extends Component {
 
   render() {
     return(
+      <Router>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         <Footer />
       </div>
+      </Router>
     );
   }
 
